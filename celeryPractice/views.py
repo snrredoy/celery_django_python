2f7d89040f53d5f75f94c8cfac3b84d99a284c51
snrredoy <snrredoy3@gmail.com>
2025-05-31 13:59:12 +0600

    set task.py to add shared task

--- celeryPractice/views.py
+++ celeryPractice/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from project.celery import add
+from .tasks import sub
 
 # Create your views here.
 def home(request):
-    result = add.delay(4, 4)
-    print("Task is running in the background", result)
+    result_add = add.delay(4, 4)
+    result_sub = sub.delay(4, 4)
+    print("Task is running in the background", result_add, result_sub)
     return render(request, 'home.html')
